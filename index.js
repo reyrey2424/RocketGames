7e6142bfddd5ccb283fe86bcafbadb70fa525d1d
reyrey2424 <reyrey2424@icloud.com>
2025-03-12 14:46:25 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,96 +1,112 @@
-
+import wisp from "wisp-server-node";
+import { createBareServer } from "@tomphttp/bare-server-node";
+import httpProxy from "http-proxy";
+import chalk from "chalk";
+import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
+import { libcurlPath } from "@mercuryworkshop/libcurl-transport";
+import { epoxyPath } from "@mercuryworkshop/epoxy-transport";
+import { bareModulePath } from "@mercuryworkshop/bare-as-module3";
+import { baremuxPath } from "@mercuryworkshop/bare-mux/node";
 import express from "express";
-import http from "node:http";
-import createBareServer from "@tomphttp/bare-server-node";
-import path from "node:path";
-import * as dotenv from "dotenv";
-dotenv.config();
+import { createServer } from "node:http";
+import { join } from "node:path";
+import packageJson from "./package.json" with { type: "json" };
+import { fileURLToPath } from "node:url";
 
-const __dirname = process.cwd();
-const server = http.createServer();
-const app = express(server);
-const bareServer = createBareServer("/bare/");
-const PORT = process.env.PORT || 8080
+const cdnProxy = httpProxy.createProxyServer();
+const bare = createBareServer("/bare/");
+const __dirname = join(fileURLToPath(import.meta.url), "..");
+const app = express();
+const publicPath = "public";
+app.set("views", join(__dirname, publicPath, "html"));
 
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-
-app.use(express.static(path.join(__dirname, "static")));
+app.use(express.static(publicPath));
+app.use("/uv/", express.static(uvPath));
+app.use("/epoxy/", express.static(epoxyPath));
+app.use("/baremux/", express.static(baremuxPath));
+app.use("/libcurl/", express.static(libcurlPath));
+app.use("/bareasmodule/", express.static(bareModulePath));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "index.html"));
+	res.sendFile(join(__dirname, publicPath, "html", "index.html"));
+});
+app.use("/cdn", (req, res) => {
+	cdnProxy.web(req, res, {
+		target: "https://gms.parcoil.com/",
+		changeOrigin: true,
+	});
+});
+app.get("/science", (req, res) => {
+	res.sendFile(join(__dirname, publicPath, "html", "games.html"));
+});
+app.get("/play", (req, res) => {
+	res.sendFile(join(__dirname, publicPath, "html", "play.html"));
+});
+app.get("/forum*/*", (req, res) => {
+	res.sendFile(join(__dirname, publicPath, "html", "forum.html"));
 });
 app.get("/math", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "games.html"));
-});
-app.get("/english", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "apps.html"));
-});
-app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "about.html"));
+	res.sendFile(join(__dirname, publicPath, "html", "apps.html"));
 });
 app.get("/settings", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "settings.html"));
+	res.sendFile(join(__dirname, publicPath, "html", "settings.html"));
 });
-app.get("/changelog", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "changelog.html"));
+app.get("/go", (req, res) => {
+	res.sendFile(join(__dirname, publicPath, "html", "go.html"));
 });
-app.get("/portal", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "loader.html"));
+app.get("/package.json", (req, res) => {
+	res.json(packageJson);
 });
-app.get("/dashboard", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "agloader.html"));
+app.get("*", (req, res) => {
+	res.sendFile(join(__dirname, publicPath, "html", "404.html"));
 });
-app.get("/snorlax", (req, res) => {
-  res.sendFile(path.join(__dirname, "static/people-secrets/", "snorlax.html"));
-});
-app.get("/tlochsta", (req, res) => {
-  res.sendFile(path.join(__dirname, "static/people-secrets/", "tlochsta.html"));
-});
-app.get("/fowntain", (req, res) => {
-  res.sendFile(path.join(__dirname, "static/people-secrets/", "fowntain.html"));
-});
-app.get("/bigfoot", (req, res) => {
-  res.sendFile(path.join(__dirname, "static/people-secrets/", "bigfoot.html"));
-});
-app.get("/burb", (req, res) => {
-  res.sendFile(path.join(__dirname, "static/people-secrets/", "burb.html"));
-});
-app.get("/derpman", (req, res) => {
-  res.sendFile(path.join(__dirname, "static/people-secrets/", "derpman.html"));
-});
-app.get("/cats", (req, res) => {
-  res.sendFile(path.join(__dirname, "static/people-secrets/", "cats.html"));
-});
-app.use((req, res) => {
-  res.statusCode = 404;
-  res.sendFile(path.join(__dirname, './static/404.html'))
-});
+const server = createServer();
 
 server.on("request", (req, res) => {
-  if (bareServer.shouldRoute(req)) {
-    bareServer.routeRequest(req, res);
-  } else {
-    app(req, res);
-  }
+	if (bare.shouldRoute(req)) {
+		bare.routeRequest(req, res);
+	} else {
+		app(req, res);
+	}
 });
 
 server.on("upgrade", (req, socket, head) => {
-  if (bareServer.shouldRoute(req)) {
-    bareServer.routeUpgrade(req, socket, head);
-  } else {
-    socket.end();
-  }
+	if (req.url.endsWith("/wisp/")) {
+		wisp.routeRequest(req, socket, head);
+	} else if (bare.shouldRoute(req)) {
+		bare.routeUpgrade(req, socket, head);
+	} else {
+		socket.end();
+	}
 });
+
+let port = parseInt(process.env.PORT || "");
+
+if (isNaN(port)) port = 8080;
 
 server.on("listening", () => {
-  console.log(`Snorlax's Cave listening on port ${process.env.PORT}`);
+	const address = server.address();
+	console.log("Listening on:");
+	console.clear();
+	console.log(
+		chalk.green(`🚀 Lunaar V6 Listening on http://localhost:${address.port}`),
+	);
+	console.log();
+	console.log(chalk.magenta(`🌙 Made by the Parcoil Network`));
+	console.log();
+	console.log(chalk.white(`⭐ Please Star on github`));
 });
 
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+function shutdown() {
+	console.log("SIGTERM signal received: closing HTTP server");
+	server.close();
+	bare.close();
+	process.exit(0);
+}
+
 server.listen({
-  port: PORT,
+	port,
 });
