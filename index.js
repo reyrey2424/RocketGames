b26a73f2b6c975eb802cf79af7e08973337a7144
reyrey2424 <reyrey2424@icloud.com>
2025-03-12 14:57:33 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,112 +1,46 @@
-import wisp from "wisp-server-node";
-import { createBareServer } from "@tomphttp/bare-server-node";
-import httpProxy from "http-proxy";
-import chalk from "chalk";
-import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
-import { libcurlPath } from "@mercuryworkshop/libcurl-transport";
-import { epoxyPath } from "@mercuryworkshop/epoxy-transport";
-import { bareModulePath } from "@mercuryworkshop/bare-as-module3";
-import { baremuxPath } from "@mercuryworkshop/bare-mux/node";
-import express from "express";
-import { createServer } from "node:http";
-import { join } from "node:path";
-import packageJson from "./package.json" with { type: "json" };
-import { fileURLToPath } from "node:url";
+const express = require("express");
+const http = require("http");
+const { createBareServer } = require("@tomphttp/bare-server-node");
+const path = require("path");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const cdnProxy = httpProxy.createProxyServer();
-const bare = createBareServer("/bare/");
-const __dirname = join(fileURLToPath(import.meta.url), "..");
 const app = express();
-const publicPath = "public";
-app.set("views", join(__dirname, publicPath, "html"));
+const port = process.env.PORT || 8080;
 
-app.use(express.static(publicPath));
-app.use("/uv/", express.static(uvPath));
-app.use("/epoxy/", express.static(epoxyPath));
-app.use("/baremux/", express.static(baremuxPath));
-app.use("/libcurl/", express.static(libcurlPath));
-app.use("/bareasmodule/", express.static(bareModulePath));
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-	res.sendFile(join(__dirname, publicPath, "html", "index.html"));
-});
-app.use("/cdn", (req, res) => {
-	cdnProxy.web(req, res, {
-		target: "https://gms.parcoil.com/",
-		changeOrigin: true,
-	});
-});
-app.get("/science", (req, res) => {
-	res.sendFile(join(__dirname, publicPath, "html", "games.html"));
-});
-app.get("/play", (req, res) => {
-	res.sendFile(join(__dirname, publicPath, "html", "play.html"));
-});
-app.get("/forum*/*", (req, res) => {
-	res.sendFile(join(__dirname, publicPath, "html", "forum.html"));
-});
-app.get("/math", (req, res) => {
-	res.sendFile(join(__dirname, publicPath, "html", "apps.html"));
-});
-app.get("/settings", (req, res) => {
-	res.sendFile(join(__dirname, publicPath, "html", "settings.html"));
-});
-app.get("/go", (req, res) => {
-	res.sendFile(join(__dirname, publicPath, "html", "go.html"));
-});
-app.get("/package.json", (req, res) => {
-	res.json(packageJson);
-});
-app.get("*", (req, res) => {
-	res.sendFile(join(__dirname, publicPath, "html", "404.html"));
-});
-const server = createServer();
+const routes = [
+  { path: "/", file: "index.html" },
+  { path: "/g", file: "gms.html" },
+  { path: "/a", file: "aps.html" },
+  { path: "/s", file: "settings.html" },
+  { path: "/p", file: "proxy.html" },
+  { path: "/404", file: "404.html" },
+];
 
-server.on("request", (req, res) => {
-	if (bare.shouldRoute(req)) {
-		bare.routeRequest(req, res);
-	} else {
-		app(req, res);
-	}
+routes.forEach((route) => {
+  app.get(route.path, (req, res) => {
+    res.sendFile(path.join(__dirname, "public", route.file));
+  });
 });
 
-server.on("upgrade", (req, socket, head) => {
-	if (req.url.endsWith("/wisp/")) {
-		wisp.routeRequest(req, socket, head);
-	} else if (bare.shouldRoute(req)) {
-		bare.routeUpgrade(req, socket, head);
-	} else {
-		socket.end();
-	}
+app.use((req, res) => {
+  res.redirect("/404");
 });
 
-let port = parseInt(process.env.PORT || "");
-
-if (isNaN(port)) port = 8080;
-
-server.on("listening", () => {
-	const address = server.address();
-	console.log("Listening on:");
-	console.clear();
-	console.log(
-		chalk.green(`🚀 Lunaar V6 Listening on http://localhost:${address.port}`),
-	);
-	console.log();
-	console.log(chalk.magenta(`🌙 Made by the Parcoil Network`));
-	console.log();
-	console.log(chalk.white(`⭐ Please Star on github`));
+const bareServer = createBareServer("/b/");
+const server = http.createServer((req, res) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
 });
 
-process.on("SIGINT", shutdown);
-process.on("SIGTERM", shutdown);
-
-function shutdown() {
-	console.log("SIGTERM signal received: closing HTTP server");
-	server.close();
-	bare.close();
-	process.exit(0);
-}
-
-server.listen({
-	port,
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
--- node_modules/@mercuryworkshop/bare-as-module3/README.md
+++
@@ -1,5 +0,0 @@
-# Bare Client (as a bare-mux module)
-
-This package implements the [TompHTTP Bare Server](https://github.com/tomphttp/specifications/blob/master/BareServer.md) as a client for [Bare-Mux](https://github.com/MercuryWorkshop/bare-mux).
-
-See the bare-mux documentation for more info, and the bare server documentation too
